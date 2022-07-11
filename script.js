0907205d815d860414d7c96295eb5994b5e77785
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2022-07-11 02:41:45 -0600

    Final commit
    The data is being fetched from the api and is being showed,
    
    Note : There is no styling done for this project.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_KEY = '51373948adcb795d6e1cf8d1dd961ed1'
 
 window.onload = () =>{
 
+    
     let location
 
     let getWeather = position =>{
@@ -13,11 +14,10 @@ window.onload = () =>{
         .then(jsonData =>{
             console.log(jsonData)
 
-        //    document.getElementById('text_location').innerHTML = jsonData.name
-        //    document.getElementById('text_location_country').innerHTML = jsonData.sys.country
-        //    document.getElementById('text_temp').innerHTML = Math.round(jsonData.min.temp)
-        //    document.getElementById('text_feelslike').innerHTML = Math.round(jsonData.min.feels_like)
-        //    document.getElementById('text_desc').innerHTML = jsonData.weather[0].description
+           document.getElementById('text_location').innerHTML = jsonData.name
+           document.getElementById('text_temp').innerHTML = Math.round(jsonData.main.temp)
+           document.getElementById('text_feelslike').innerHTML = Math.round(jsonData.main.feels_like)
+           document.getElementById('text_desc').innerHTML = jsonData.weather[0].description
         })
     }
     
